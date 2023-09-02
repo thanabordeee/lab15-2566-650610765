f671214a649a01e1f573f9f4bb4777d515149b38
thanabordeee <jejekup00123@hotmail.co.th>
2023-09-02 18:38:37 +0700

    complete

--- src/app/page.js
+++ src/app/page.js
@@ -184,7 +184,7 @@ export default function Home() {
           </Stack>
         </form>
 
-        <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" />
+        <Footer year={2023} fullName="Thanabordee Sornchai" studentId="650610765" />
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
